eeeb8e12c52986f95dbfb15cfff7b140cac851f8
Matthew Iannucci <mpiannucci@gmail.com>
2015-04-07 21:31:11 -0400

    Added reference image and change source directory name

--- source/main.c
+++ source/main.c
@@ -4,11 +4,13 @@ int main(void) {
     unsigned int count;
     WDTCTL = WDTPW + WDTHOLD; //Stop watchdog timer
 
-    P1OUT = 0; //Configure P1 to output on P1.0
+    //Configure P1 to output on P1.0
+    P1OUT = 0;
     P1DIR |= 0x01;
 
     for(;;) {
-        P1OUT ^= 0x01; //Toggle P1.0 using exclusive-OR
+        //Toggle P1.0 using exclusive-OR
+        P1OUT ^= 0x01;
         for(count=0; count<20000; count++) {
             /* Insert some delay */
         }
