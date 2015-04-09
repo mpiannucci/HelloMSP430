486fa33065ef22e89a61569b05615865e5297c8e
Matthew Iannucci <miannucci@navatekltd.com>
2015-04-09 11:39:03 -0400

    Fix linux msp430 gcc path

--- cmake/msp430.cmake
+++ cmake/msp430.cmake
@@ -10,16 +10,19 @@ if(CMAKE_HOST_APPLE)
 elseif(CMAKE_HOST_WIN32)
     set(MSP430_PATH C:/Users/miannucci/msp430)
 else()
-    set(MSP430_PATH /usr/local/msp430)
+    set(MSP430_PATH /usr/msp430)
 endif()
 
 # Specify the cross compiler
 if(CMAKE_HOST_WIN32)
     CMAKE_FORCE_C_COMPILER(${MSP430_PATH}/bin/msp430-gcc.exe GNU)
     CMAKE_FORCE_CXX_COMPILER(${MSP430_PATH}/bin/msp430-g++.exe GNU)
-else()
+elseif(CMAKE_HOST_APPLE)
     CMAKE_FORCE_C_COMPILER(${MSP430_PATH}/bin/msp430-gcc GNU)
     CMAKE_FORCE_CXX_COMPILER(${MSP430_PATH}/bin/msp430-g++ GNU)
+else()
+    CMAKE_FORCE_C_COMPILER(msp430-gcc GNU)
+    CMAKE_FORCE_CXX_COMPILER(msp430-g++ GNU)
 endif()
 
 # Where is the target environment located
@@ -33,7 +36,11 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # Include The header directory
-include_directories(SYSTEM ${MSP430_PATH}/msp430/include)
+if (CMAKE_HOST_APPLE OR CMAKE_HOST_WIN32)
+    include_directories(SYSTEM ${MSP430_PATH}/msp430/include)
+else()
+    include_directories(SYSTEM ${MSP430_PATH}/include)
+endif()
 
 # Set the compiler flags (REQUIRED). This is defined here and not the toolchain file 
 # so the toolchain file can be re used for multiple boards
@@ -43,7 +50,11 @@ set(CMAKE_EXE_LINKER_FLAGS ${CPU_FLAG})
 
 # Create a function that will instantiate a flash target command using mspdebug.
 # you may have to set the path to mspdebug if it is different
-set(MSPDEBUG_PATH ${MSP430_PATH}/mspdebug/mspdebug)
+if (CMAKE_HOST_APPLE)
+    set(MSPDEBUG_PATH ${MSP430_PATH}/mspdebug/mspdebug)
+else()
+    set(MSPDEBUG_PATH mspdebug)
+endif()
 function(setup_flash_target TARGET_NAME DRIVER)
     add_custom_target(flash
         COMMAND ${MSPDEBUG_PATH} ${DRIVER} 'prog ${TARGET_NAME}'
