e063615ee79bbf4e5cab64c6a5f1b0bcabee0c91
Matthew Iannucci <mpiannucci@gmail.com>
2015-04-24 14:21:35 -0400

    Fix comment formatting

--- source/dht11.h
+++ source/dht11.h
@@ -30,18 +30,21 @@ static void dht11_send_start_signal(void);
 
 /**
  * Get the response to the start signal being received by the device
+ * 
  * @return  1 if ready to send data, 0 otherwise
  */
 static unsigned int dht11_check_response(void);
 
 /**
  * Read a data byte from the device. Assumes the start signal and rcheck response have both been executed
+ * 
  * @return  The byte read from the device
  */
 static unsigned char dht11_read_byte(void);
 
 /**
  * Verify that the data was received correctly.
+ * 
  * @return  1 if the data received matches the checksum, 0 otherwise
  */
 unsigned int dht11_verify_checksum(void);
@@ -51,6 +54,7 @@ unsigned int dht11_verify_checksum(void);
  * This function will send the start signal, check the response, and then read the data into a DHT11_Data
  * structure. This way, the user can simply call one function to receive all data. If you want to verify the
  * data, call dht_verify_checksum after this function, but before reading another packet.
+ * 
  * @return  A data structure holding the data received from the device
  */
 DHT11_Data dht11_get_data(void);
