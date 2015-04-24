561a22ca8e4303f46699734eb4f02f36c9d8e3f6
Matthew Iannucci <mpiannucci@gmail.com>
2015-04-24 16:55:27 -0400

    Formatted some of the comments

--- source/led.h
+++ source/led.h
@@ -18,7 +18,8 @@ void led_toggle_red_state(void);
 void led_set_red_state(int state);
 
 /**
- * Get the state of the red led
+ * Get the state of the red led\
+ * 
  * @param state 0 for off, Non zero for on.
  */
 int led_get_red_state(void);
@@ -30,12 +31,14 @@ void led_toggle_green_state(void);
 
 /**
  * Set the state of the green led
+ * 
  * @param state 0 for off, 1 for on.
  */
 void led_set_green_state(int state);
 
 /**
  * Get the state of the green led
+ * 
  * @return state 0 for off, Non zero for on
  */
 int led_get_green_state(void);
