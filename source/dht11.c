91837da400841a4ebfea5af86a986e8fd185e22d
Matthew Iannucci <mpiannucci@gmail.com>
2015-04-23 22:35:27 -0400

    Add function skeletons

--- source/dht11.c
+++ source/dht11.c
@@ -1,2 +1,37 @@
 #include "dht11.h"
 #include <msp430.h>
+
+void dht11_init() {
+    // Set the time to 5 Hz
+    TACCR0 = 50000;
+
+    // Enable interrupt
+    TACCTL0 = CCIE;
+
+    // SMCLK, div 4, up mode,
+    TA0CTL = TASSEL_2 + ID_2 + MC_1 + TACLR;
+}
+
+void dht11_set_timer_isr_ptr(void (*isr_ptr)(void)) {
+
+}
+
+void dht11_send_start_signal(void) {
+
+}
+
+unsigned int dht11_check_response(void) {
+    return 1;
+}
+
+unsigned int dht11_read_byte(void) {
+    return 1;
+}
+
+unsigned int dht11_verify_checksum() {
+    return 1;
+}
+
+unsigned int* dht11_get_data(void) {
+    return 0;
+}
--- source/dht11.h
+++ source/dht11.h
@@ -1,6 +1,18 @@
 #ifndef DHT11_H
 #define DHT11_H
 
+void dht11_init(void);
 
+void dht11_set_timer_isr_ptr(void (*isr_ptr)(void));
+
+static void dht11_send_start_signal(void);
+
+static unsigned int dht11_check_response(void);
+
+static unsigned int dht11_read_byte(void);
+
+static unsigned int dht11_verify_checksum(void);
+
+unsigned int* dht11_get_data(void);
 
 #endif // DHT11_H
