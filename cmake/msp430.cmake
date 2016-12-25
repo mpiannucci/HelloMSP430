03a430a39f926c14b46b7ab4c7ce918078099783
Matthew Iannucci <mpiannucci@gmail.com>
2016-12-25 16:16:29 -0500

    Switch linux toolchain to use energia installed in /opt

--- cmake/msp430.cmake
+++ cmake/msp430.cmake
@@ -30,19 +30,16 @@ if(CMAKE_HOST_APPLE)
 elseif(CMAKE_HOST_WIN32)
     set(MSP430_PATH C:/Users/miannucci/msp430)
 else()
-    set(MSP430_PATH /usr/msp430)
+    set(MSP430_PATH /opt/energia/hardware/tools/msp430)
 endif()
 
 # Specify the cross compiler
 if(CMAKE_HOST_WIN32)
     set(CMAKE_C_COMPILER ${MSP430_PATH}/bin/msp430-gcc.exe)
     set(CMAKE_CXX_COMPILER ${MSP430_PATH}/bin/msp430-g++.exe)
-elseif(CMAKE_HOST_APPLE)
+else()
     set(CMAKE_C_COMPILER ${MSP430_PATH}/bin/msp430-gcc)
     set(CMAKE_CXX_COMPILER ${MSP430_PATH}/bin/msp430-g++)
-else()
-    set(CMAKE_C_COMPILER msp430-gcc)
-    set(CMAKE_CXX_COMPILER msp430-g++)
 endif()
 
 # Where is the target environment located
@@ -56,19 +53,12 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # Include The header directory
-if (CMAKE_HOST_APPLE OR CMAKE_HOST_WIN32)
-    include_directories(SYSTEM ${MSP430_PATH}/msp430/include)
-else()
-    include_directories(SYSTEM ${MSP430_PATH}/include)
-endif()
+include_directories(SYSTEM ${MSP430_PATH}/msp430/include)
 
 # Create a function that will instantiate a flash target command using mspdebug.
 # you may have to set the path to mspdebug if it is different
-if (CMAKE_HOST_APPLE)
-    set(MSPDEBUG_PATH ${MSP430_PATH}/bin/mspdebug)
-else()
-    set(MSPDEBUG_PATH mspdebug)
-endif()
+set(MSPDEBUG_PATH ${MSP430_PATH}/bin/mspdebug)
+
 function(setup_flash_target TARGET_NAME DRIVER)
     add_custom_target(flash
         COMMAND ${MSPDEBUG_PATH} ${DRIVER} 'prog ${TARGET_NAME}'
